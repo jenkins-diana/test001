8a37bcdde204afc1fbceba76857defade6d628d7
Mitsuharu UEMOTO <uemoto@ccs.tsukuba.ac.jp>
2017-06-02 17:03:56 +0900

    ファイル追加

---
+++ code002/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+/* This source code raise warning message
+   because of main routine will not returns value */
+
+int main(int argc, char** argv) {
+  /* Example */
+  printf("Hello World\n");
+  /* return 0; */
+}
