1f1fbb9b61d9257d7ba1f2b1cc52e02e431f0b28
Mitsuharu UEMOTO <uemoto@ccs.tsukuba.ac.jp>
2017-06-03 11:29:33 +0900

    いろいろ追加

---
+++ code003/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/* This code raise the fatal error, due to the lack of function defination */
+
+/*
+int add(int x, int y) {
+  return x + y;
+}
+*/
+
+int main(int argc, char** argv) {
+  /* Example */
+  printf("Hello World %d\n", add(2, 3s));
+  return 0;
+}
